1e7f910d9ead56001ce8f43cc6d81e3377d334a5
aidiary <f2forest@gmail.com>
2017-04-29 21:44:50 +0900

    Add test_cifar10.py

---
+++ test_cifar10.py
@@ -0,0 +1,33 @@
+import os
+
+import chainer
+import chainer.links as L
+from net import SiameseNetwork
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 訓練済みモデルをロード
+model = SiameseNetwork()
+chainer.serializers.load_npz(os.path.join('result_cifar10', 'model.npz'), model)
+
+# テストデータをロード
+_, test = chainer.datasets.get_cifar10(ndim=3)
+test_data, test_label = test._datasets
+
+# テストデータを学習した低次元空間（2次元）に写像
+y = model.forward_once(test_data)
+feat = y.data
+
+# ラベルごとに描画
+c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff',
+     '#ff00ff', '#990000', '#999900', '#009900', '#009999']
+
+# 各ラベルごとに異なる色でプロット
+# 同じクラス内のインスタンスが近くに集まり、
+# 異なるクラスのインスタンスが離れていれば成功
+for i in range(10):
+    f = feat[np.where(test_label == i)]
+    plt.plot(f[:, 0], f[:, 1], '.', c=c[i])
+plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+plt.savefig(os.path.join('result_cifar10', 'result.png'))
